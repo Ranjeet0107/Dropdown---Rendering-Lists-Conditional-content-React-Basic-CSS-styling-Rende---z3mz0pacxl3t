dc1c18bc9706405842e0e090dd445d41b34e7e0a
Ranjeet <ranjeet.ggi@gmail.com>
2022-12-06 15:02:13 +0530

    new commit

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card=({title, name, description }) =>{
+    return(
+        <article style={{
+            border:"1px solid #c1c1c1",
+            padding:"0.5rem",
+            margin:"0.5rem 0"
+        }}
+        
+        >
+            <h3 id={`${title}-title`}>{name}</h3>
+            <p id={`${title}-description`}>{description}</p>
+        </article>
+    )
+}
+
+
+export default Card;
